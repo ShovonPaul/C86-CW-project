a252465ac13954645ddde716cb8b31eea2d22585
ShovonPaul <110727330+ShovonPaul@users.noreply.github.com>
2023-05-21 15:39:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC1z6kghFb_tqfieRnN49LNUugM2JJp-3s",
+    authDomain: "storytellingapp-96282.firebaseapp.com",
+    projectId: "storytellingapp-96282",
+    storageBucket: "storytellingapp-96282.appspot.com",
+    messagingSenderId: "456176450499",
+    appId: "1:456176450499:web:6e67053ab986a8230ddb76"
+  };
